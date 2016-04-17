1c827eee74be33609bab79cb3bd60a5ccd8af3c8
jslmac <jsl@mac>
2016-04-18 07:42:08 +0900

    wk7 6_dt added

---
+++ src/w3MainNoHelp.py
@@ -0,0 +1,16 @@
+def sayHello():
+    print "I say hello!"
+
+def sayHelloHelp(name):
+    print name, "say hello!"
+
+def lab3():
+    sayHello()
+    sayHelloHelp("jsl")
+
+def main():
+    lab3()
+
+if __name__=="__main__":
+    main()
+
---
+++ src/w3MainNoHelpNoMain.py
@@ -0,0 +1,12 @@
+def sayHello():
+    print "I say hello!"
+
+def sayHelloHelp(name):
+    print name, "say hello!"
+
+def lab3():
+    sayHello()
+    sayHelloHelp("jsl")
+
+lab3()
+
